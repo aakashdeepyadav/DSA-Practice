10998dfbdc6b5819d50b39be9fd3d422abb80add
Aakash Deep Yadav <114255488+aakashdeepyadav@users.noreply.github.com>
2025-10-21 22:37:43 +0530

    Refactor searchInsertPosition and remove binarySearch
    Refactored the searchInsertPosition function to return the correct insert position directly. Removed binarySearch function as it was no longer needed.

--- Array/searchInsertPosition.cpp
+++ Array/searchInsertPosition.cpp
@@ -2,7 +2,8 @@
 #include<vector>
 using namespace std;
 
-int binarySearch(vector<int>&arr, int target){
+
+int searchInsertPosition(vector<int>&arr, int target){
     int size=arr.size();
     int low=0;
     int high=size-1;
@@ -17,38 +18,15 @@ int binarySearch(vector<int>&arr, int target){
             high=mid-1;
         }
     }
-    return -1;
-}
 
-
-int searchInsertPosition(vector<int>&arr, int target){
-    int size=arr.size();
-    int low=0;
-    int high=size-1;
-    while(low<=high){
-        int mid=(low+high)/2;
-        
-        if(arr[mid]<target && arr[mid+1]>target){
-            return mid+1;
-        }else if(arr[mid]<target){
-            low=mid+1;
-        }else{
-            high=mid-1;
-        }
-    }
-
-    return -1;
+    return low;
 }
 
 int main(){
     vector<int> arr={1,2,3,4,4,5,8};
     int target=7;
-
-    int SearchedIndex=binarySearch(arr, target);
-    if(SearchedIndex==-1){
-        SearchedIndex=searchInsertPosition(arr,target);
-    }
-
+    int SearchedIndex=searchInsertPosition(arr,target);
+    
     cout << "The searched Insert Position is at index " << SearchedIndex;
 
     return 0;
