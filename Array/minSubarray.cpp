50e2c1f5e176d3db6a7f6fd5e081c73be8e199bf
Aakash Deep Yadav <aakashdeepyadav106@gmail.com>
2025-11-30 23:58:08 +0530

    adding todays problem of the day of leetcode

---
+++ Array/minSubarray.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int minSubarray(vector<int> vec, int p)
+{
+    long long total = 0;
+    for (int x : vec)
+        total += x;
+
+    int rem = total % p;
+    if (rem == 0)
+        return 0;
+
+    unordered_map<long long, int> mp;
+    mp[0] = -1;
+
+    long long prefix = 0;
+    int n = vec.size();
+    int ans = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        prefix = (prefix + vec[i]) % p;
+        long long need = (prefix - rem + p) % p;
+
+        if (mp.count(need))
+            ans = min(ans, i - mp[need]);
+
+        mp[prefix] = i;
+    }
+
+    return ans == n ? -1 : ans;
+}
+
+int main()
+{
+    vector<int> vecArray={3,5,6,7};
+    int k=8;
+
+    int subarray=minSubarray(vecArray,k);
+    cout << "The minimum subarray is " << subarray;
+    return 0;
+}
